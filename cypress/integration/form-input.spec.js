889c74be405fec1fa484d526b5d92d47aa0d1bbf
FranSantos10 <fran.hagelin@gmail.com>
2023-05-03 22:47:48 -0300

    criação de teste

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,12 +1,20 @@
 describe('Form input', ()=> {
+    beforeEach(()=>{
+        cy.visit('/');
+    })
+
     //teste 1
     it('Focus no input ao carregar', ()=>{
-        cy.visit('/');
         cy.focused().should('have.id','title');
     });
+
     //teste 2
-    it.only('aceita input do usuario', ()=> {
-        cy.visit('/');
-        cy.get('#title').type('nova tarefa')
-    })
+    it('aceita input do usuario', ()=> {//it.only para rodar somente este teste
+        const valor = 'New task'
+
+        cy.get('#title')
+        .type(valor)
+        .should('have.value',valor);
+    });
+
 })
